47fcb3168dc88284cfdd43e12cf4cdbac351592d
Renney Thomas <renneyt@gmail.com>
2013-02-05 11:38:32 -0500

    Added Console App.

---
+++ ConsoleApplicationRemoteGitHub/ConsoleApplicationRemoteGitHub/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplicationRemoteGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World.");
+        }
+    }
+}
